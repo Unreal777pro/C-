8ea38a2a02ac639a4b147a1b97ea3d5f3768a49f
Unreal <unreal777x@gmail.com>
2022-07-07 21:11:43 +0300

    Example008 IntroMethod

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+int a1 = 23;
+int b1 = 9;
+int c1 = 11;
+
+int a2 = 19;
+int b2 = 33;
+int c2 = 111;
+
+int a3 = 22;
+int b3 = 33;
+int c3 = 1;
+
+int max = Max(Max(a1,b1,c1),Max(a2,b2,c2), Max(a3,b3,c3));
+Console.WriteLine("Максимальное значение = " + max);
