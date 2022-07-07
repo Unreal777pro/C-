837ac86bb483fde28a4cbe68ad22fa2eeb8bb9b3
Unreal <unreal777x@gmail.com>
2022-07-07 21:28:22 +0300

    Work003

---
+++ Work003/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа получает на вход число и проверяет, является ли оно четным
+
+int Number = new Random().Next(10001);
+
+if (Number%2==0) Console.WriteLine(Number + " ->да");
+else Console.WriteLine(Number + " ->нет");
+
+//альтернативно
+
+//string Even = "нет";
+//if (Number%2==0) Even = "да";
+//Console.WriteLine(Number + "->" + Even);
