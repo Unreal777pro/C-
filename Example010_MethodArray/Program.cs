c86abc4a2f9ea85c5b605cf15a0349fa411264d0
Unreal <unreal777x@gmail.com>
2022-07-07 21:17:33 +0300

    Example010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {9,8,6,4,2,11,14,16};
+int ValueToFind = 4;
+int ArrayLength = array.Length;
+int Index = 0;
+
+while (Index < ArrayLength)
+{
+    if (array[Index] == ValueToFind) 
+    {
+        Console.WriteLine(Index);
+        break;
+    }
+    Index++;
+}
