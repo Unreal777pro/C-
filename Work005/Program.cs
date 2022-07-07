aabdbfebe198e7d451d11fabae6df2e40aa0452f
Unreal <unreal777x@gmail.com>
2022-07-07 21:37:42 +0300

    .\Work005\

---
+++ Work005/Program.cs
@@ -0,0 +1,8 @@
+﻿//программа принимает на вход трехзначное число и на выходе показывает вторую цифру числа
+
+Console.WriteLine("Введите трехзначное положительное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit = Number%100/10;
+
+Console.WriteLine($"{Number} -> {SecondDigit}");
