2844e003695df9d415b9b3335a756593a11a29b1
Unreal <unreal777x@gmail.com>
2022-07-07 21:42:49 +0300

    .\Work006\

---
+++ Work006/Program.cs
@@ -0,0 +1,16 @@
+﻿//программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int Number = new Random().Next(1,100000);
+
+if (Number < 100) Console.WriteLine(Number + "-> Третьей цифры нет");
+else 
+{
+    int ThirdDigit = Number;
+
+    while (ThirdDigit > 999) 
+    {
+        ThirdDigit = ThirdDigit/10;
+    }
+    
+    Console.WriteLine(Number + "-> " + ThirdDigit%10);
+}
