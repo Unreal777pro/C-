8053a6e94c9d0435035c8da4450fa2fe64fea319
Unreal <unreal777x@gmail.com>
2022-07-07 21:46:34 +0300

    .\Work007\

---
+++ Work007/Program.cs
@@ -0,0 +1,7 @@
+﻿//программа принимает на вход цифру, обозначающую день недели и проверяет, является ли этот день выходным
+
+Console.WriteLine("Введите номер дня недели: ");
+int DayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (DayNumber > 5) Console.WriteLine(DayNumber + "-> да");
+else Console.WriteLine(DayNumber + "-> нет");;
