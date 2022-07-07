6f4263a3eabeda267627ec3eb7e5519457864946
Unreal <unreal777x@gmail.com>
2022-07-07 21:23:04 +0300

    Work001

---
+++ Work001/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа принимает на вход 2 числа и сообщает какое из них больше, а какое меньше
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) Console.WriteLine("a=" + a +"; b=" + b + " -> max=" + a);
+if (a<b) Console.WriteLine("a=" + a +"; b=" + b + " -> max=" + b);
+if (a==b) Console.WriteLine("a=" + a +"; b=" + b + " -> числа равны");
