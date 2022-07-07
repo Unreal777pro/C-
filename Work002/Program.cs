5984d53c33b2256a222100caaee4c4ef15abb2e6
Unreal <unreal777x@gmail.com>
2022-07-07 21:25:33 +0300

    Work002

---
+++ Work002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа принимает на вход 3 числа и выдает максимальное из них
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.WriteLine(a + "," + b + "," + c + " ->" + max);
